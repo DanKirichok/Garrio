fcef24ee7e5c1e471b241f6ab699fef4adbad704
DanDaMan0411 <dan.kirichok@gmail.com>
2016-06-29 14:16:23 -0400

    Made design look a little better. Can search for other users based on username. Added support to change bio and profile pic. On edit profile menu, the previously set values automatically are set on the edit profile page.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,15 @@ function getProfilePic(picName){
 	document.getElementsByClassName("profile_pic")[0].src = "/profile_pics/" + picName
 }
 
+//This presets the bio to the one the user has in edit profile page
 function updateBio(bio){
 	document.getElementById("update_bio_text").value = bio
+}
+
+//This presets the profile pic to the one the user has in edit profile page
+function updateProfilePic(profile_pic){
+	var picName = profile_pic.split(".")[0];
+	var picNum = picName.slice(3, picName.length);
+	
+	document.getElementsByName("profile_pic_selector")[0].selectedIndex = picNum - 1
 }
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,7 @@ router.get('/', ensureAuthenticated, function(req, res){
 router.get('/edit_profile', ensureAuthenticated, function(req, res){	
 	var context = {
 		bio: req.user.bio,
+		profile_pic: req.user.profile_pic,
 	}
 	
 	res.render('edit_profile', context);
@@ -29,9 +30,7 @@ router.get('/edit_profile', ensureAuthenticated, function(req, res){
 router.post('/', ensureAuthenticated, function(req, res){	
 	var bio = req.body.bio;
 	var profile_pic = req.body.profile_pic_selector;
-	
-	console.log(profile_pic);
-	
+		
 	//This updates the bio of the user to what the user had input into the form
 	User.update({_id: req.user._id}, {$set:{bio: bio}}, function(err, result){
 		if (err) throw err;
@@ -51,6 +50,40 @@ router.post('/', ensureAuthenticated, function(req, res){
 	}
 	
 	res.render('index', context)
+});
+
+//Posts the submitted data in edit profile
+router.post('/user_result', ensureAuthenticated, function(req, res){		
+	var username = req.body.username
+	
+	var first_name;
+	var last_name;
+	var profile_pic;
+	var bio;
+	
+	User.getUserByUsername(username, function(err, user){
+		if(err) throw err;
+		if (user != null){
+			first_name = user.first_name;
+			last_name = user.last_name;
+			profile_pic = user.profile_pic;
+			bio = user.bio;
+						
+			var context = {
+				username: username,
+				first_name: first_name,
+				last_name: last_name,
+				username: username,
+				profile_pic: profile_pic,
+				bio: bio,
+			}
+			
+			res.render('user_result', context)
+		}else{
+			req.flash('error_msg', username + ' was not found.');
+			res.redirect('/');
+		}
+	})
 });
 
 function ensureAuthenticated(req, res, next){
