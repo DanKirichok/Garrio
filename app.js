353ce0e672f71887d7c6039b00e486f69d5f8e3a
DanDaMan0411 <dan.kirichok@gmail.com>
2016-07-14 20:40:34 -0400

    Added support for online mongodb when I want to run on openshift server. Also changed package.json so it wouldn't cause errors when running on openshift.

--- app.js
+++ app.js
@@ -11,7 +11,20 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+//Development Setting
 mongoose.connect('mongodb://localhost/socialNetwork');
+
+//Connects to online db. Used for production
+/*
+mongoose.connect('mongodb://dan:daniscool123@ds017165.mlab.com:17165/garrio', function(err){
+	console.log("Connecting to mongoose...");
+	if (err){
+		console.log("There is an error connecting to the db");
+		console.log(err)
+	};
+});
+*/
+
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
@@ -78,7 +91,6 @@ app.use(function (req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 
-/*
 //Development settings
 // Set Port
 app.set('port', (process.env.PORT || 4000));
@@ -86,13 +98,13 @@ app.set('port', (process.env.PORT || 4000));
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
 });
-*/
 
+/*
 //This is for openshift
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.listen(port, ipaddress, function(){
-	console.log('listening on *:4000')
+app.listen(port, ipaddress, function(){
+	console.log('Using Production settings')
 })
-
+*/
